efd694f777666cca4d362f07ca4b5d1c35d4a212
alkuhaliff99ff99-a11y <alkuhaliff99ff99@gmail.com>
2026-07-09 22:06:23 +0300

    تحديث .gitignore لتجاهل مجلد البناء target

--- target/debug/deps/fsl-6576969f864ccad7.d
+++
@@ -1,5 +0,0 @@
-/data/data/com.termux/files/home/Failang/target/debug/deps/fsl-6576969f864ccad7.d: src/main.rs
-
-/data/data/com.termux/files/home/Failang/target/debug/deps/libfsl-6576969f864ccad7.rmeta: src/main.rs
-
-src/main.rs:
--- target/debug/deps/fsl-96739643ec028571.d
+++
@@ -1,8 +0,0 @@
-/data/data/com.termux/files/home/Failang/target/debug/deps/fsl-96739643ec028571.d: src/lib.rs src/compiler/mod.rs src/compiler/lexer/mod.rs src/compiler/lexer/tokens.rs
-
-/data/data/com.termux/files/home/Failang/target/debug/deps/libfsl-96739643ec028571.rmeta: src/lib.rs src/compiler/mod.rs src/compiler/lexer/mod.rs src/compiler/lexer/tokens.rs
-
-src/lib.rs:
-src/compiler/mod.rs:
-src/compiler/lexer/mod.rs:
-src/compiler/lexer/tokens.rs:
